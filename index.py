f0e08348a6ec48ff441eff62926b108f0d29290c
saurabh gupta <saurabh.guptathoughtworks.com@saurabhs-MacBook-Pro.local>
2021-05-16 01:30:58 +0530

    test

--- index.py
+++ index.py
@@ -2,8 +2,15 @@ import time
 
 from pushbullet import Pushbullet
 
-print("Hello world!")
 #
 # while(True):
 #     Pushbullet("o.IH0xSegxyprlnyGm3Qdnx9ZvlKrcnL6c").push_note('Hi,', 'this is an alert!')
 #     time.sleep(10)
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
