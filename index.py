04949ae6a4aa7f4ac46d308d1b550441f1e35dce
saurabh gupta <saurabh.guptathoughtworks.com@saurabhs-MacBook-Pro.local>
2021-05-16 01:18:59 +0530

    test

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ import time
 
 from pushbullet import Pushbullet
 
-while(True):
-    Pushbullet("o.IH0xSegxyprlnyGm3Qdnx9ZvlKrcnL6c").push_note('Hi,', 'this is an alert!')
-    time.sleep(10)
+print("Hello world!")
+#
+# while(True):
+#     Pushbullet("o.IH0xSegxyprlnyGm3Qdnx9ZvlKrcnL6c").push_note('Hi,', 'this is an alert!')
+#     time.sleep(10)
